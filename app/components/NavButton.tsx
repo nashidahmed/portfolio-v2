1fe32a891aa2e8e4dd8dec0d61eabc0e5dc6b939
Nashid Ahmed <nashid.ahmed.115@gmail.com>
2025-02-02 21:47:23 -0500

    Updated nav button a tag to link

--- app/components/NavButton.tsx
+++ app/components/NavButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 interface NavButtonProps {
   title: string;
@@ -10,38 +11,39 @@ interface NavButtonProps {
 
 export default function NavButton({ title, link }: NavButtonProps) {
   return (
-    <motion.a
-      href={link}
-      className="relative flex cursor-pointer flex-col items-center justify-center"
-      initial="initial"
-      whileHover="hover"
-    >
-      {/* Default State (White Text) */}
-      <motion.span
-        className="flex-1 text-6xl font-semibold text-primaryBg"
-        variants={{
-          initial: { opacity: 1 },
-          hover: { opacity: 0 },
-        }}
-        transition={{ duration: 0.2 }}
-      >
-        {title}
-      </motion.span>
-
-      {/* Hover State (Bold Red with Arrow) */}
+    <Link href={link}>
       <motion.div
-        className="absolute flex items-center justify-center"
-        variants={{
-          initial: { opacity: 0 },
-          hover: { opacity: 1 },
-        }}
-        transition={{ duration: 0.3 }}
+        className="relative flex cursor-pointer flex-col items-center justify-center"
+        initial="initial"
+        whileHover="hover"
       >
-        <span className="text-secondaryText text-6xl font-extrabold">
+        {/* Default State (White Text) */}
+        <motion.span
+          className="flex-1 text-6xl font-semibold text-primaryBg"
+          variants={{
+            initial: { opacity: 1 },
+            hover: { opacity: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+        >
           {title}
-        </span>
-        <span className="text-secondaryText text-6xl">↓</span>
+        </motion.span>
+
+        {/* Hover State (Bold Red with Arrow) */}
+        <motion.div
+          className="absolute flex items-center justify-center"
+          variants={{
+            initial: { opacity: 0 },
+            hover: { opacity: 1 },
+          }}
+          transition={{ duration: 0.3 }}
+        >
+          <span className="text-secondaryText text-6xl font-extrabold">
+            {title}
+          </span>
+          <span className="text-secondaryText text-6xl">↓</span>
+        </motion.div>
       </motion.div>
-    </motion.a>
+    </Link>
   );
 }
